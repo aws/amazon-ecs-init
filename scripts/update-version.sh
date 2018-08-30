30bb596584926ebd4345bae5bed05ee65b90e8d8
nightkhaos <nightkhaos+gpg@gmail.com>
2018-08-30 01:13:13 +0000

    Revert "fixed version directory for duplicate named file and fixed dependencies in Makefile"
    This reverts commit c71856a226e2264aa6af91a49ded9b1deeb3d328.

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -17,7 +17,7 @@
 set -e -x
 
 CURRENTDIR=$(dirname "${0}")
-version=$(cat "${CURRENTDIR}"/../ecs-init/ECSVERSION)
+version=$(cat "${CURRENTDIR}"/../ecs-init/VERSION)
 git_hash=$(git rev-parse --short=8 HEAD)
 git_dirty=false
 
